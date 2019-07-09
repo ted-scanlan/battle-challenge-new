f0705dabd075a9975d884eddcb3fe0b47b7b0758
Student <student@Makerss-MacBook-Pro.local>
2019-07-09 16:52:39 +0100

    view hit points completed

--- app.rb
+++ app.rb
@@ -5,14 +5,26 @@ require 'rspec'
 
 
 class Battle < Sinatra::Base
+  enable :sessions
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @nameone = params[:nameone]
-    @nametwo = params[:nametwo]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
+
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    @player_1_points = 60
+    @player_2_points = 60
     erb :play
+
+
   end
 
   run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -11,11 +11,30 @@ feature 'Enable players to enter names' do
   scenario 'testing name input from form' do
     visit('/')
     expect(page).to have_content('Enter Names')
-    fill_in 'nameone', with: 'Bob'
-    fill_in 'nametwo', with: 'Dave'
+    fill_in 'player_1', with: 'Bob'
+    fill_in 'player_2', with: 'Dave'
     click_button 'Submit'
     expect(page).to have_content "Welcome to Battle, Bob & Dave!"
   end
 
-  
+
+end
+
+feature 'hit points' do
+  scenario 'can display player 1 hit points' do
+    visit('/')
+    expect(page).to have_content('Enter Names')
+    fill_in 'player_1', with: 'Bob'
+    click_button 'Submit'
+      expect(page).to have_content "Bob HB points = 60"
+
+  end
+  scenario 'can display player 2 hit points' do
+    visit('/')
+    expect(page).to have_content('Enter Names')
+    fill_in 'player_2', with: 'Dave'
+    click_button 'Submit'
+      expect(page).to have_content "Dave HB points = 60"
+
+  end
 end
